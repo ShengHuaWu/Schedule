e94b2eb36ba6c929193c52e3140aa29218cac72d
ShengHua Wu <fantasy0404@gmail.com>
2017-06-10 18:02:42 +0100

    Project initialization.

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by ShengHua Wu on 10/06/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
